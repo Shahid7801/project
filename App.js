c2a14117609c964641fc9f31697eac16267f2493
Shahid7801 <shahidraza7801@gmail.com>
2024-09-02 02:23:10 +0530

    app.js

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "./PromoCard.jsx";
+import { PromoCard } from "./PromoCard";
+
+
+function App(pricee1, price2) {
+  return (
+    <div>
+ 
+    <PromoCard price1="6000" price2="9000"></PromoCard>
+  
+    </div>
+  );
+}
+export default App
+
+
+
+
+
+
